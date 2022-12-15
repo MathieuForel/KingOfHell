006b99dd05da4d31cfe6973ed94774176bbf9b1a
Azzrenoza <116853920+MathieuForel@users.noreply.github.com>
2022-12-15 14:41:34 +0100

    bug mouvement

--- Assets/TileMap/Scripts/Move.cs
+++ Assets/TileMap/Scripts/Move.cs
@@ -5,31 +5,8 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
-    {/*
-        if(collision.gameObject.layer == 10)
-        {
-            if(collision.gameObject.GetComponentInParent<TileState>().isWater)
-            {
-                for (int i = 0; i < this.gameObject.transform.parent.childCount; i++)
-                {
-                    if (this.gameObject.transform.localPosition.x > 0)
-                    {
-                        if (this.gameObject.transform.parent.GetChild(i).localPosition.x > this.gameObject.transform.localPosition.x &&
-                            this.gameObject.transform.parent.GetChild(i).localPosition.y == this.gameObject.transform.localPosition.y)
-                        {
-                            this.gameObject.transform.parent.GetChild(i).GetChild(0).gameObject.SetActive(false);
-                        }
-                    }
-                    else
-                    {
-                        if (this.gameObject.transform.parent.GetChild(i).localPosition.x < this.gameObject.transform.localPosition.x)
-                        {
-                            this.gameObject.transform.parent.GetChild(i).GetChild(0).gameObject.SetActive(false);
-                        }
-                    }
-                }
-        }*/
+    public void LateUpdate()
+    {
+        this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
     }
-
 }
